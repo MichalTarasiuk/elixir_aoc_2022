debcc28fa7fe3091fc7772e0a2fb54bf0b2ecfd8
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2023-04-08 17:47:23 +0200

    first commit

---
+++ lib/elixir_aoc2022.ex
@@ -0,0 +1,2 @@
+defmodule ElixirAoc2022 do
+end
---
+++ test/elixir_aoc2022_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirAoc2022Test do
+  use ExUnit.Case
+  doctest ElixirAoc2022
+
+  test "greets the world" do
+    assert ElixirAoc2022.hello() == :world
+  end
+end
