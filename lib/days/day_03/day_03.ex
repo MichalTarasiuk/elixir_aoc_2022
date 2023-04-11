19efa964dd457f305ae567eef54ec817fca4f794
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2023-04-11 22:39:22 +0200

    add solution for second part of third day

--- lib/days/day_03/day_03.ex
+++ lib/days/day_03/day_03.ex
@@ -16,11 +16,17 @@ defmodule ElixirAoc2022.Day03 do
     |> String.split("\n")
   end
 
-  defp intersection(list1, list2) do
+  defp intersection_2(list1, list2) do
     m1 = MapSet.new(list1)
     m2 = MapSet.new(list2)
 
     MapSet.intersection(m1, m2) |> MapSet.to_list()
+  end
+
+  defp intersection(lists) do
+    {acc, other_lists} = List.pop_at(lists, 0, [])
+
+    List.foldl(other_lists, acc, &intersection_2(&1, &2))
   end
 
   def solve_part_1 do
@@ -34,9 +40,19 @@ defmodule ElixirAoc2022.Day03 do
       first_compartment = Enum.slice(splitted_rucksack, 0..(divider - 1))
       second_compartment = Enum.slice(splitted_rucksack, divider..length(splitted_rucksack))
 
-      letter = intersection(first_compartment, second_compartment) |> Enum.at(0)
+      letter = intersection_2(first_compartment, second_compartment) |> Enum.at(0)
 
       acc + @letter_to_point[letter]
     end)
+  end
+
+  def solve_part_2 do
+    splitted_input = get_splitted_input()
+
+    splitted_input
+    |> Enum.chunk_every(3, 3, :discard)
+    |> Enum.map(&Enum.map(&1, fn rucksack -> String.split(rucksack, "", trim: true) end))
+    |> Enum.flat_map(&intersection(&1))
+    |> List.foldl(0, &(@letter_to_point[&1] + &2))
   end
 end
