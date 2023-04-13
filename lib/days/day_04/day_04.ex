d4ddbf3461e8ea921d3954aa60bfb5dff9c4b061
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2023-04-13 07:46:46 +0200

    add first solution of 04 day

---
+++ lib/days/day_04/day_04.ex
@@ -0,0 +1,18 @@
+defmodule ElixirAoc2022.Day04 do
+  defp pair_to_range_tuple(pair) do
+    String.split(pair, ",")
+    |> Enum.map(&(String.split(&1, "-") |> Enum.map(fn part -> String.to_integer(part) end)))
+  end
+
+  defp superset_subset([[a, b], [x, y]]) do
+    (a <= x and b >= y) or (a >= x and b <= y)
+  end
+
+  def solve_part_1 do
+    splitted_input = ElixirAoc2022.Utils.get_splitted_input("/lib/days/day_04/input.txt")
+
+    splitted_input
+    |> Enum.map(&pair_to_range_tuple(&1))
+    |> Enum.count(&superset_subset(&1))
+  end
+end
