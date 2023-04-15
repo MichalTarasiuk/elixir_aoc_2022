f4880706637708cdce12845718886d67be067708
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2023-04-15 22:12:53 +0200

    add solution of second part of sixth day

--- lib/days/day_06/day_06.ex
+++ lib/days/day_06/day_06.ex
@@ -3,20 +3,28 @@ defmodule ElixirAoc2022.Day06 do
     length(list) != list |> Enum.uniq() |> length()
   end
 
-  def solve_part_1 do
+  def find_marker(marker_length) do
     input = ElixirAoc2022.Utils.read_input("/lib/days/day_06/input.txt")
     splitted_input = String.split(input, "", trim: true)
 
     splitted_input
     |> Enum.with_index(fn _value, index -> index end)
     |> Enum.find_index(fn index ->
-      slice = Enum.slice(splitted_input, (index - 3)..index)
+      slice = Enum.slice(splitted_input, (index - (marker_length - 1))..index)
 
       cond do
-        length(slice) == 4 -> not has_duplicates(slice)
+        length(slice) == marker_length -> not has_duplicates(slice)
         true -> false
       end
     end)
     |> then(&(&1 + 1))
+  end
+
+  def solve_part_1 do
+    find_marker(4)
+  end
+
+  def solve_part_2 do
+    find_marker(14)
   end
 end
