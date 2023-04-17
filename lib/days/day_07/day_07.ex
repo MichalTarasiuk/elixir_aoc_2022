6a5d05b5c6ae987b0824ae6802495afde8e77701
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2023-04-18 00:28:20 +0200

    add basic parser for 07 day

--- lib/days/day_07/day_07.ex
+++ lib/days/day_07/day_07.ex
@@ -2,64 +2,53 @@ defmodule ElixirAoc2022.Day07 do
   @size_limit 100_000
 
   @file_regexp ~r/(\d+)\s\w+(\.\w+)?/
-  @directory_regxp ~r/dir\s(\w+)/
+  @directory_regexp ~r/dir\s(\w+)/
 
-  defp parse_input(input) do
-    input
-    |> String.replace(~r/\$ cd (\w+|\/)/, "$ cd\n\\g{1}")
-    |> String.split(~r/\$ cd(\s..)?\n/, trim: true)
-    |> Enum.map(&(String.split(&1, ~r/\n\$ ls\n/) |> List.to_tuple()))
-    |> Map.new()
-  end
+  @initial_path ""
 
-  defp parse_directory_value(directories, directory_value) do
-    is_file = String.match?(directory_value, @file_regexp)
-    is_directory = String.match?(directory_value, @directory_regxp)
+  @move_back_cmd_regexp ~r/(\/\w+$)/
+  @move_forward_cmd_regexp ~r/^\$ cd (\/|(\w+))$/
+  @move_back_cmd "$ cd .."
 
-    case true do
-      ^is_file ->
-        String.replace(directory_value, @file_regexp, "\\g{1}") |> String.to_integer()
-
-      ^is_directory ->
-        String.replace(directory_value, @directory_regxp, "\\g{1}")
-        |> then(fn directory_name ->
-          directory_line = Map.get(directories, directory_name)
-
-          if directory_line, do: parse_directory_values(directories, directory_line), else: 0
-        end)
-
-      true ->
-        0
+  defp add_prefix(str, prefix) do
+    cond do
+      String.length(str) != 0 -> "#{prefix}#{str}"
+      true -> str
     end
   end
 
-  defp parse_directory_values(directories, directory_line) do
-    directory_values = String.split(directory_line, "\n", trim: true)
-
-    directory_values
-    |> Enum.map(&parse_directory_value(directories, &1))
-    |> List.flatten()
-    |> Enum.sum()
+  defp move_back(path) do
+    path
+    |> String.replace(@move_back_cmd_regexp, "")
   end
 
-  defp get_size_of_directories(directories) do
-    directories
-    |> Map.keys()
-    |> Enum.map(fn directory_name ->
-      directory_line = Map.fetch!(directories, directory_name)
+  defp is_move_forward_cmd(element) do
+    String.match?(element, @move_forward_cmd_regexp)
+  end
 
-      directory_line
-      |> then(&parse_directory_values(directories, &1))
-      |> then(&{directory_name, &1})
+  defp move_forward(current_path, cmd) do
+    name = String.replace(cmd, @move_forward_cmd_regexp, "\\g{2}")
+
+    current_path <> add_prefix(name, "/")
+  end
+
+  def parse_input do
+    splitted_input = ElixirAoc2022.Utils.get_splitted_input("/lib/days/day_07/input.txt")
+
+    splitted_input
+    |> Enum.reduce({@initial_path, %{}}, fn element, {path, directories} ->
+      back_path = move_back(path)
+
+      cond do
+        element == @move_back_cmd ->
+          {back_path, Map.put_new(directories, back_path, [])}
+
+        is_move_forward_cmd(element) ->
+          {move_forward(path, element), Map.put_new(directories, move_forward(path, element), [])}
+
+        true ->
+          {path, directories}
+      end
     end)
-    |> Map.new()
-  end
-
-  def solve_part_1 do
-    input = ElixirAoc2022.Utils.read_input("/lib/days/day_07/input.txt")
-
-    input
-    |> parse_input()
-    |> get_size_of_directories()
   end
 end
