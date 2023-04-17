be7b550d2a39d3a34f8de173e08f8c97cd2db3b7
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2023-04-17 08:34:01 +0200

    add parse_directory_value method for day -7

--- lib/days/day_07/day_07.ex
+++ lib/days/day_07/day_07.ex
@@ -1,7 +1,10 @@
 defmodule ElixirAoc2022.Day07 do
   @size_limit 100_000
 
-  def parse_input(input) do
+  @file_regexp ~r/(\d+)\s\w+(\.\w+)?/
+  @directory_regxp ~r/dir\s(\w+)/
+
+  defp parse_input(input) do
     input
     |> String.replace(~r/\$ cd (\w+|\/)/, "$ cd\n\\g{1}")
     |> String.split(~r/\$ cd(\s..)?\n/, trim: true)
@@ -9,10 +12,39 @@ defmodule ElixirAoc2022.Day07 do
     |> Map.new()
   end
 
+  defp parse_directory_value(directories, directory_value) do
+    is_file = String.match?(directory_value, @file_regexp)
+    is_directory = String.match?(directory_value, @directory_regxp)
+
+    case true do
+      ^is_file ->
+        String.replace(directory_value, @file_regexp, "\\g{1}") |> String.to_integer()
+
+      ^is_directory ->
+        String.replace(directory_value, ~r/dir\s(\w+)/, "\\g{1}")
+
+      true ->
+        0
+    end
+  end
+
+  defp get_size_of_directories(directories) do
+    directories
+    |> Map.keys()
+    |> Enum.map(fn directory_name ->
+      directory_values = Map.fetch!(directories, directory_name)
+
+      directory_values
+      |> String.split("\n", trim: true)
+      |> Enum.map(&parse_directory_value(directories, &1))
+    end)
+  end
+
   def solve_part_1 do
     input = ElixirAoc2022.Utils.read_input("/lib/days/day_07/input.txt")
 
     input
     |> parse_input()
+    |> get_size_of_directories()
   end
 end
